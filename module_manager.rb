942490c8b3a3fc797870fba4af71ed4062270836
sarahemm <github@sen.cx>
2018-08-15 16:21:13 -0400

    Reworked how modules are organized internally a bit.
    This is more framework for revamping how all the UI modules fit together and allowing multiple modules of one type to be loaded at once.

--- module_manager.rb
+++ module_manager.rb
@@ -7,13 +7,29 @@ class TimerModule
     # register a class as an external module
     def register(category, name, instance)
       @modules = Hash.new if !@modules
-      @modules[category] = Hash.new if !@modules.key? category
+      @modules[category] = Collection.new if !@modules.key? category
       @modules[category][name] = instance
     end
     
     def [](category)
       return if !@modules
       @modules[category]
+    end
+  end
+
+  class Collection
+    def initialize
+      @members = Hash.new
+      puts "Initializing new module collection."
+    end
+
+    def +(instance)
+      @members[@members.length] = instance
+    end
+
+    def []=(name, instance)
+      @members[name] = instance
+      puts "Collection item #{name} set to #{instance}."
     end
   end
 end
