e50d02b568574f54aa574be468a2b7a5f3ca16ee
sarahemm <github@sen.cx>
2018-08-15 21:07:14 -0400

    Migrated screen modules to the new module manager system.

--- ui_screen_console.rb
+++ ui_screen_console.rb
@@ -1,22 +1,26 @@
-class TimerUI
-  class Screen
-    def initialize
-    end
+class TimerModule
+  class ScreenModule
+    class Console
+      def initialize
+      end
     
-    def home
-      puts "---"
-    end
+      def home
+        puts "---"
+      end
     
-    def clear
-      puts "# Clear Screen"
-    end
+      def clear
+        puts "# Clear Screen"
+      end
     
-    def write(text)
-      puts text
-    end
+      def write(text)
+        puts text
+      end
     
-    def background_colour(colour)
-      puts "# Background color: #{colour}"
+      def background_colour(colour)
+        puts "# Background color: #{colour}"
+      end
     end
   end
 end
+
+TimerModule::Manager.instance.register(:screen, :console, TimerModule::ScreenModule::Console.new)
--- ui_screen_lcd.rb
+++ ui_screen_lcd.rb
@@ -1,47 +1,51 @@
 require 'lcd/char16x2'
 
-class TimerUI
-  class Screen
-    def initialize()
-      @lcd = Adafruit::LCD::Char16x2.new
-      @lcd.backlight(Adafruit::LCD::Char16x2::RED)
-      @lcd.clear
-    end
-    
-    def home
-      @lcd.home
-    end
-    
-    def clear
-      @lcd.clear
-    end
-    
-    def write(text)
-      @lcd.message text
-    end
-    
-    def background_colour(colour)
-      colour_code = case(colour)
-        when :black
-          Adafruit::LCD::Char16x2::OFF
-        when :red
-          Adafruit::LCD::Char16x2::RED
-        when :green
-          Adafruit::LCD::Char16x2::GREEN
-        when :blue
-          Adafruit::LCD::Char16x2::BLUE
-        when :yellow
-          Adafruit::LCD::Char16x2::YELLOW
-        when :teal
-          Adafruit::LCD::Char16x2::TEAL
-        when :purple
-          Adafruit::LCD::Char16x2::VIOLET
-        when :white
-          Adafruit::LCD::Char16x2::WHITE
-        else
-          Adafruit::LCD::Char16x2::OFF
+class TimerModule
+  class ScreenModule
+    class LCD
+      def initialize()
+        @lcd = Adafruit::LCD::Char16x2.new
+        @lcd.backlight(Adafruit::LCD::Char16x2::RED)
+        @lcd.clear
       end
-      @lcd.backlight colour_code
+    
+      def home
+        @lcd.home
+      end
+    
+      def clear
+        @lcd.clear
+      end
+    
+      def write(text)
+        @lcd.message text
+      end
+    
+      def background_colour(colour)
+        colour_code = case(colour)
+          when :black
+            Adafruit::LCD::Char16x2::OFF
+          when :red
+            Adafruit::LCD::Char16x2::RED
+          when :green
+            Adafruit::LCD::Char16x2::GREEN
+          when :blue
+            Adafruit::LCD::Char16x2::BLUE
+          when :yellow
+            Adafruit::LCD::Char16x2::YELLOW
+          when :teal
+            Adafruit::LCD::Char16x2::TEAL
+          when :purple
+            Adafruit::LCD::Char16x2::VIOLET
+          when :white
+            Adafruit::LCD::Char16x2::WHITE
+          else
+            Adafruit::LCD::Char16x2::OFF
+        end
+        @lcd.backlight colour_code
+      end
     end
   end
 end
+
+TimerModule::Manager.instance.register(:screen, :lcd, TimerModule::ScreenModule::LCD.new)
